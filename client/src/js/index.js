439bc7018ddd9174ac7950a9bef7734572504b4b
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-14 18:25:05 -0700

    Cleaned the code a little

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -18,30 +18,6 @@ const loadSpinner = () => {
   main.appendChild(spinner);
 };
 
-// const initializeEditor = async () => {
-//   try {
-//     const editor = new Editor();
-//     await editor.initialize();
-//     main.innerHTML = ''; // Clear the loading spinner or any previous content
-//     main.appendChild(editor.editor); // Append the editor to the DOM
-
-//     // Load all items from IndexedDB
-//     const allData = await getDb();
-//     console.info('Loaded data from IndexedDB, injecting into editor');
-    
-//     // Set the value of the editor to all loaded data
-//     editor.editor.setValue(allData.join('\n'));
-
-//   } catch (error) {
-//     console.error('Error initializing editor:', error);
-//     // Handle the error, perhaps display an error message
-//   }
-// };
-
-
-// loadSpinner();
-
-// initializeEditor();
 const editor = new Editor()
 if (typeof editor === "undefined") {
   loadSpinner()
